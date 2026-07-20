8e4914308f6238719dbe7a26fa958ad2d501387e
Kaosar Ahmed <kaosar1971@gmail.com>
2026-07-21 02:20:21 +0600

    Configure project for deployment

--- CaloryCounter/settings.py
+++ CaloryCounter/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/"static"
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 #REST FRAMEWORK
 
@@ -148,6 +149,14 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "dashboard"
+LOGOUT_REDIRECT_URL = "login"
